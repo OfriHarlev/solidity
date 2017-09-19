0c8a8b1ac3e75a759d016b3bca228df14dfa2935
chriseth <chris@ethereum.org>
2017-09-19 22:37:17 +0200

    Tests

--- test/libsolidity/ABIDecoderTests.cpp
+++ test/libsolidity/ABIDecoderTests.cpp
@@ -227,45 +227,167 @@ BOOST_AUTO_TEST_CASE(byte_arrays)
 			}
 		}
 	)";
-	int enc= 0;
 	BOTH_ENCODERS(
 		compileAndRun(sourceCode);
 		bytes args = encodeArgs(
 			6, 0x60, 9,
 			7, "abcdefg"
 		);
-	cout << enc++ << endl;
-	cout << toHexAndSplit(callContractFunction("f(uint256,bytes,uint256)", args)) << endl;
 		BOOST_CHECK(
 			callContractFunction("f(uint256,bytes,uint256)", args) ==
-			encodeArgs(u256(6), u256(7), 'd')
+			encodeArgs(u256(6), u256(7), "d", 9)
 		);
 		BOOST_CHECK(
 			callContractFunction("f_external(uint256,bytes,uint256)", args) ==
-			encodeArgs(u256(6), u256(7), 'd')
+			encodeArgs(u256(6), u256(7), "d", 9)
 		);
 	)
 }
 
-// dynamic nested arrays
+BOOST_AUTO_TEST_CASE(decode_from_memory_simple)
+{
+	string sourceCode = R"(
+		contract D {
+			uint public _a;
+			uint[] public _b;
+			function D(uint a, uint[] b) {
+				_a = a;
+				_b = b;
+			}
+		}
+		contract C is D {
+			function C(uint a, uint[] b) D(a, b) { }
+		}
+	)";
+	BOTH_ENCODERS(
+		compileAndRun(sourceCode, 0, "C", encodeArgs(
+			7, 0x40,
+			// b
+			3, 0x21, 0x22, 0x23
+		));
+		BOOST_CHECK(callContractFunction("_a()") == encodeArgs(7));
+		BOOST_CHECK(callContractFunction("_b(uint256)", 0) == encodeArgs(0x21));
+		BOOST_CHECK(callContractFunction("_b(uint256)", 1) == encodeArgs(0x22));
+		BOOST_CHECK(callContractFunction("_b(uint256)", 2) == encodeArgs(0x23));
+		BOOST_CHECK(callContractFunction("_b(uint256)", 3) == encodeArgs());
+	)
+}
 
-// calldata types
-// decoding from memory (used for constructor, especially forwarding to base constructor)
-// test about incorrect input length
-// test cleanup inside arrays and structs
+BOOST_AUTO_TEST_CASE(decode_from_memory_complex)
+{
+	string sourceCode = R"(
+		contract D {
+			uint public _a;
+			uint[] public _b;
+			bytes[2] public _c;
+			function D(uint a, uint[] b, bytes[2] c) {
+				_a = a;
+				_b = b;
+				_c = c;
+			}
+		}
+		contract C is D {
+			function C(uint a, uint[] b, bytes[2] c) D(a, b, c) { }
+		}
+	)";
+	NEW_ENCODER(
+		compileAndRun(sourceCode, 0, "C", encodeArgs(
+			7, 0x60, 7 * 0x20,
+			// b
+			3, 0x21, 0x22, 0x23,
+			// c
+			0x40, 0x80,
+			8, string("abcdefgh"),
+			52, string("ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ")
+		));
+		BOOST_CHECK(callContractFunction("_a()") == encodeArgs(7));
+		BOOST_CHECK(callContractFunction("_b(uint256)", 0) == encodeArgs(0x21));
+		BOOST_CHECK(callContractFunction("_b(uint256)", 1) == encodeArgs(0x22));
+		BOOST_CHECK(callContractFunction("_b(uint256)", 2) == encodeArgs(0x23));
+		BOOST_CHECK(callContractFunction("_b(uint256)", 3) == encodeArgs());
+		BOOST_CHECK(callContractFunction("_c(uint256)", 0) == encodeArgs(0x20, 8, string("abcdefgh")));
+		BOOST_CHECK(callContractFunction("_c(uint256)", 1) == encodeArgs(0x20, 52, string("ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ")));
+		BOOST_CHECK(callContractFunction("_c(uint256)", 2) == encodeArgs());
+	)
+}
+
+BOOST_AUTO_TEST_CASE(short_input_value_type)
+{
+	string sourceCode = R"(
+		contract C {
+			function f(uint a) returns (uint) { return a; }
+		}
+	)";
+	NEW_ENCODER(
+		compileAndRun(sourceCode);
+		BOOST_CHECK(callContractFunction("f(uint256)", 1) == encodeArgs(1));
+		BOOST_CHECK(callContractFunction("f(uint256)", bytes(31, 0)) == encodeArgs(0));
+	)
+}
+
+BOOST_AUTO_TEST_CASE(short_input_array)
+{
+	string sourceCode = R"(
+		contract C {
+			function f(uint[] a) returns (uint) { return 7; }
+		}
+	)";
+	NEW_ENCODER(
+		compileAndRun(sourceCode);
+		BOOST_CHECK(callContractFunction("f(uint256[])", 0x20, 0) == encodeArgs(7));
+		BOOST_CHECK(callContractFunction("f(uint256[])", 0x20, 1) == encodeArgs());
+		BOOST_CHECK(callContractFunction("f(uint256[])", 0x20, 2, 5, 6) == encodeArgs(7));
+	)
+}
+
+BOOST_AUTO_TEST_CASE(short_input_bytes)
+{
+	string sourceCode = R"(
+		contract C {
+			function f(bytes[] a) returns (uint) { return 7; }
+		}
+	)";
+	NEW_ENCODER(
+		compileAndRun(sourceCode);
+		BOOST_CHECK(callContractFunction("f(bytes[])", 0x20, 1, 0x20, 7, bytes(7, 0)) == encodeArgs(7));
+		BOOST_CHECK(callContractFunction("f(bytes[])", 0x20, 1, 0x20, 7, bytes(6, 0)) == encodeArgs());
+	)
+}
+
+BOOST_AUTO_TEST_CASE(cleanup_int_inside_arrays)
+{
+	string sourceCode = R"(
+		contract C {
+			enum E { A, B }
+			function f(uint16[] a) pure returns (uint r) { assembly { r := mload(add(a, 0x20)) } }
+			function g(int16[] a) pure returns (uint r) { assembly { r := mload(add(a, 0x20)) } }
+			function h(E[] a) pure returns (uint r) { assembly { r := mload(add(a, 0x20)) } }
+		}
+	)";
+	NEW_ENCODER(
+		compileAndRun(sourceCode);
+		BOOST_CHECK(callContractFunction("f(uint16[])", 0x20, 1, 7) == encodeArgs(7));
+		BOOST_CHECK(callContractFunction("g(int16[])", 0x20, 1, 7) == encodeArgs(7));
+		BOOST_CHECK(callContractFunction("f(uint16[])", 0x20, 1, u256("0xffff")) == encodeArgs(u256("0xffff")));
+		BOOST_CHECK(callContractFunction("g(int16[])", 0x20, 1, u256("0xffff")) == encodeArgs(u256(-1)));
+		BOOST_CHECK(callContractFunction("f(uint16[])", 0x20, 1, u256("0x1ffff")) == encodeArgs(u256("0xffff")));
+		BOOST_CHECK(callContractFunction("g(int16[])", 0x20, 1, u256("0x10fff")) == encodeArgs(u256("0x0fff")));
+		BOOST_CHECK(callContractFunction("h(uint8[])", 0x20, 1, 0) == encodeArgs(u256(0)));
+		BOOST_CHECK(callContractFunction("h(uint8[])", 0x20, 1, 1) == encodeArgs(u256(1)));
+		BOOST_CHECK(callContractFunction("h(uint8[])", 0x20, 1, 2) == encodeArgs());
+	)
+}
+
 // test decoding storage pointers
 
-// btye arrays
+// test that byte arrays in pure calldata and properly decoded into memory
+// are valid, even if they do not have padding (i.e. the length check does not throw)
 
-// test that calldata types are decoded efficiently, i.e. no element access for bytes,
-// and perhaps also no element access for uint256
-
-// TODO: For decoding from memory, we might want to avoid a copy. Could that pose a problem?
 // TODO: ridiculously sized arrays, also when the size comes from deeply nested "short" arrays
 
 // check again in the code that "offset" is always compared to "end"
 
-// structs
+// structs, cleanup inside strcuts
 // combination of structs, arrays and value types
 
 BOOST_AUTO_TEST_SUITE_END()
